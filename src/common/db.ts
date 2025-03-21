53046ad87b00fb7cc8c1a65791ab9df37a2212f1
Aditya Veer Parmar <nakulparmar15@gmail.com>
2025-03-21 14:07:53 +0530

    sqlite support

--- src/common/db.ts
+++ src/common/db.ts
@@ -1,10 +1,17 @@
 import { isNull } from '@/common/functions'
 import { AyaPostgresDatabaseAdapter } from '@/databases/postgres/adapter'
+import { SqliteDatabaseAdapter } from '@elizaos/adapter-sqlite'
 import { IDatabaseAdapter, IDatabaseCacheAdapter } from '@elizaos/core'
+import Database from 'better-sqlite3'
 
-export async function initializeDatabase(): Promise<IDatabaseAdapter & IDatabaseCacheAdapter> {
+export async function initializeDatabase(
+  dbFile: string
+): Promise<IDatabaseAdapter & IDatabaseCacheAdapter> {
   if (isNull(process.env.POSTGRES_URL)) {
-    throw new Error('POSTGRES_URL is not set')
+    const db = new Database(dbFile)
+    const adapter = new SqliteDatabaseAdapter(db)
+    await adapter.init()
+    return adapter
   }
 
   const db = new AyaPostgresDatabaseAdapter({
